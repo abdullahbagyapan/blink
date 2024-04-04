a157155040cacafc60e61b44bbaf8679c683196a
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-04-04 13:47:37 +0300

    Update::main.c
    -Add brief to bitwise operations

--- main.c
+++ main.c
@@ -4,15 +4,16 @@
 
 int main() {
 
-
+    // Set Data Direction Register to 1 (output)
     DDRB = DDRB | (1 << DDB5);
 
     while (1) 
     {
+        // Set Data Register to 1
         PORTB = PORTB | (1 << PORTB5);
         _delay_ms(1000);
 
-
+        // Set Data Register to 0
         PORTB = PORTB & ~(1 << PORTB5);
         _delay_ms(1000);
 
